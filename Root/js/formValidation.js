27bcf6c7b9438055d60e3e91a4de45361099b990
Dylan <dylanyamaguchikuan@gmail.com>
2024-03-05 18:31:29 -0600

    form validation for account creation

---
+++ Root/js/formValidation.js
@@ -0,0 +1,81 @@
+const form = document.getElementById('infoForm');
+const errorElement = document.getElementById('error');
+
+const firstnameEntry = document.getElementById('firstnameEntry');
+const lastnameEntry = document.getElementById('lastnameEntry');
+const emailEntry = document.getElementById('emailEntry');
+const idEntry = document.getElementById('idEntry');
+const passwordEntry = document.getElementById('passwordEntry');
+const confirmEntry = document.getElementById('confirmEntry');
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+    //first name field validation
+    if (firstnameEntry.value === '' || firstnameEntry === null) {
+        messages.push('First name required');
+    }
+    else if (!/^[A-Za-z]+$/.test(firstnameEntry.value)) {
+        messages.push('First name cannot contain numbers');
+    }
+
+
+
+    //last name field validation
+    if (lastnameEntry.value === '' || lastnameEntry === null) {
+        messages.push('Last name required');
+    }
+    else if (!/^[A-Za-z]+$/.test(lastnameEntry.value)) {
+        messages.push('Last name cannot contain numbers');
+    }
+
+
+
+    //email field validation
+    if (emailEntry.value === '' || emailEntry === null) {
+        messages.push('Email required');
+    }
+    else if (!/\S+@\S+\.\S+/.test(emailEntry.value)) {
+        messages.push('Invalid email format');
+    }
+
+
+
+    //student ID field validation
+    if (idEntry.value === '' || idEntry === null) {
+        messages.push('SID required');
+    }
+    else if (!/^\d{9}$/.test(idEntry.value)) {
+        messages.push('SID should be 9 digits long and contain only numbers');
+    }
+
+
+    //password field validation
+    if(passwordEntry.value === '' || passwordEntry === null) {
+        messages.push('Password required');
+    }
+    if(passwordEntry.value.length < 5) {
+        messages.push('Password must be longer than 5 characters');
+    }
+    if(passwordEntry.value.length > 20) {
+        messages.push('Password cannot be more than 20 characters');
+    }
+
+
+
+    //password confirmation field validation
+    if(confirmEntry.value ==='' || confirmEntry === null) {
+        messages.push('Password confirmation required');
+    }
+    else if (confirmEntry.value !== passwordEntry.value) {
+        messages.push('Passwords do not match');
+    }
+
+
+    //if any errors exist, will prevent form from submitting
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.style.color = 'red';
+        errorElement.style.fontWeight = 'bold';
+        errorElement.innerText = messages.join('. ');
+    }
+}) 
