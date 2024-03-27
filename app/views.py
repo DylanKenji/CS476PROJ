8898ab1ca7a8397842de5682ff0f91df3341e4c5
Ilias <ilias99892@gmail.com>
2024-03-26 21:26:32 -0600

    added backend function to delete account

--- app/views.py
+++ app/views.py
@@ -400,3 +400,29 @@ def apply_for_job():
     else:
         # Handle case where student ID or job ID is missing
         return jsonify({'error': 'Student ID or Job ID is missing'}), 400
+
+
+@app.route('/delete_account', methods=['POST'])
+def delete_account():
+    if 'student' in session:
+        student_id = session['student']
+        student = Students.query.get(student_id)
+        if student:
+            db.session.delete(student)
+            db.session.commit()
+            session.clear()
+            return render_template('home.html', message='Student deleted successfully'), 200
+        else:
+            return render_template('editStudent.html', message='Student not found'), 404
+    elif 'employer' in session:
+        employer_id = session['employer']
+        employer = Employers.query.get(employer_id)
+        if employer:
+            db.session.delete(employer)
+            db.session.commit()
+            session.clear()
+            return render_template('home.html', message='Employer deleted successfully'), 200
+        else:
+            return render_template('editEmployer.html', message='Employer not found'), 404
+    else:
+        return render_template('home.html', message='no user logged in'), 401
